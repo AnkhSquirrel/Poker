169baedf68c0e944ea52a405a5e5a29c584061ab
AnkhSquirrel <suchkp@tutanota.com>
2023-01-31 21:26:39 +0100

    console

--- src/main/kotlin/Poker.kt
+++ src/main/kotlin/Poker.kt
@@ -4,8 +4,8 @@ import views.displayWinner
 
 fun main() {
     val deck = Deck()
-    val hand1 = Hand(deck.cards.subList(0, 5).sorted())
-    val hand2 = Hand(deck.cards.subList(5, 10).sorted())
+    val hand1 = Hand(deck.cards.subList(0, 5))
+    val hand2 = Hand(deck.cards.subList(5, 10))
 
     displayWinner(hand1,hand2)
 }
--- src/main/kotlin/views/Console.kt
+++ src/main/kotlin/views/Console.kt
@@ -4,7 +4,7 @@ import HANDS
 import models.Hand
 
 fun displayWinner(hand1: Hand, hand2: Hand){
-    display(hand1,hand2)
+    display(listOf(hand1,hand2))
 
     when {
         hand1 > hand2 -> println("Hand 1 is the winner!")
@@ -13,13 +13,9 @@ fun displayWinner(hand1: Hand, hand2: Hand){
     }
 }
 
-private fun display(hand1: Hand, hand2: Hand){
-    println()
-    println("----- Hand 1 -----")
-    display(hand1)
-    println()
-    println("----- Hand 2 -----")
-    display(hand2)
+private fun display(hands: List<Hand>){
+    for (hand in hands)
+        display(hand)
 }
 
 private fun display(hand: Hand){
